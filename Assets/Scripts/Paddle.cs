d5e7553c5b3e05544f0aaa73fad37f7b41593d4f
Elena Barrachina i Ribes <ebr4@alumnes.udl.cat>
2020-07-11 16:44:55 +0200

    Started Paddle script
    · Horizontal movement (speed, axis)

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    public float speed = 7.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,5 +15,11 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float direction = Input.GetAxis("Horizontal");
+        float movement = speed * direction * Time.deltaTime; // Stable movement, non-dependant of FPS
+
+        transform.Translate(movement, 0, 0);
+        
     }
+
 }
