ae742f81dae97321524b20aa60c0f76b65caf476
Daniil Digtyar <daniildigtyar@gmail.com>
2020-07-11 19:58:28 +0200

    New ball collision

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,54 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Ball : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public Camera MainCamera; 
-    private Vector2 screenBounds;
-    private float objectWidth;
-    private float objectHeight;
+    // Movement Speed
+    public float speed = 100.0f;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
-        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; 
-        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y;
         Globals.Balls++;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
     }
 
     void OnBecameInvisible()
     {
         Globals.Balls--;
-        Destroy(this);
+        Destroy(gameObject);
     }
 
-    void LateUpdate()
-    {
-        Vector3 viewPos = transform.position;
-        Quaternion rotationPos = transform.rotation;
-        if (viewPos.x < (screenBounds.x * -1 + objectWidth))
-        {
-            print("!!1");
-            rotationPos = Quaternion.Inverse(rotationPos);
-        }
-        else if (viewPos.x > (screenBounds.x - objectWidth))
-        {
-            print("!!2");
-            rotationPos = Quaternion.Inverse(rotationPos);
-        }
-
-        //viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth , screenBounds.x - objectWidth );
-        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 - objectHeight * 2 ,  screenBounds.y + objectHeight);
-        transform.position = viewPos;
-        transform.rotation = rotationPos;
-    }
 }
