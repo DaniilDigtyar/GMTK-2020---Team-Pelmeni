5cfbaac11e05f9edc82f2641082939f1794a3f4c
Daniil Digtyar <daniildigtyar@gmail.com>
2020-07-12 16:35:05 +0200

    Main menu

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,18 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
-public class ButtonOptions : MonoBehaviour
+public class MenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickPlay()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Level");
     }
 }
