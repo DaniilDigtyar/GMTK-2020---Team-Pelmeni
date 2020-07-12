4227ec4ef3dba0c57dc952141580cf36b96fd455
Elena Barrachina i Ribes <ebr4@alumnes.udl.cat>
2020-07-12 12:09:52 +0200

    Finished Case 2
    Add an extra ball when a brick breaks.

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 { 
     public int lifespan = 1;
+    
     public Sprite BrickDamaged;
     private SpriteRenderer render;
+    
     public Ball colliderBall;
+    public GameObject extraBall;
+
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -29,7 +35,7 @@ public class Brick : MonoBehaviour
             lifespan--;
         }else if (lifespan == 0)
         {
-            switch (Random.Range(0, 1))
+            switch (Random.Range(0, 3))
             {
                 case 0:
                     //Just destroy brick
@@ -38,18 +44,24 @@ public class Brick : MonoBehaviour
                 case 1:
                     //Increase ball speed by a factor of 1.25
                     
-                    Destroy(other.gameObject);
+                    Destroy(this.gameObject);
                     
-                    //colliderBall = GameObject.FindGameObjectWithTag("Ball");
+                    //colliderBall = GameObject.FindGameObjectWithTag("ball").GetComponent<Speed>();
                     //colliderBall.SetSpeed(1000.0f);
                     break;
+                case 2:
+                    //Increase number of balls. Adds 1 ball.
+                    Destroy(this.gameObject);
+                    GameObject newBall;
+                    newBall = Instantiate(extraBall, transform.position, transform.rotation); //Try to make it appear where the previous brick was
+                    break;
             }
-            //case 2 = increase ball number
-            //case 2 = increase paddle size
-            //case 3 = decrease paddle size
-            //case 4 = change colour of bricks
-            //case 5 = rotate screen 90º
-            //case 6 = change brick distribution
+
+            //case 3 = increase paddle size
+            //case 4 = decrease paddle size
+            //case 5 = change colour of bricks
+            //case 6 = rotate screen 90º
+            //case 7 = change brick distribution
         }
     }
 }
